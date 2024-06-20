f2a39ba32844b8ea2c784493431cafadae426641
Nakshatra Goel <139595090+Nakshatra05@users.noreply.github.com>
2024-06-20 23:34:01 +0530

    Update qft.py

--- qft.py
+++ qft.py
@@ -1,1 +1,23 @@
+from qiskit import QuantumCircuit
+import numpy as np
+
+def qft(n):
+    """Creates an n-qubit Quantum Fourier Transform (QFT) circuit."""
+    qc = QuantumCircuit(n)
+    for qubit in range(n):
+        for j in range(qubit):
+            qc.cp(np.pi/float(2**(qubit-j)), j, qubit)
+        qc.h(qubit)
+    return qc
+
+def qft_dagger(n):
+    """Creates an n-qubit inverse Quantum Fourier Transform (QFT†) circuit."""
+    qc = QuantumCircuit(n)
+    for qubit in range(n//2):
+        qc.swap(qubit, n-qubit-1)
+    for j in range(n):
+        for m in range(j):
+            qc.cp(-np.pi/float(2**(j-m)), m, j)
+        qc.h(j)
+    return qc
 
